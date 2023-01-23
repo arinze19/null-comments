3acd7928afaf71d4716022ac7c63ee5c2288a93d
Arinze Obi <arinze19@outlook.com>
2023-01-23 21:04:20 +0100

    session fix

--- server/routes/AuthRoutes.js
+++ server/routes/AuthRoutes.js
@@ -10,6 +10,31 @@ class AuthRoute {
         successRedirect: config.clientUrl,
       })
     );
+    router.route('/login/success').get((req, res) => {
+      if (!req.user) {
+        res.status(401).json({
+          message: 'user cannot be authenticated',
+          success: false,
+        });
+      } else {
+        res.status(200).send({
+          success: true,
+          message: 'user authenticated successfully',
+          user: req.user,
+          cookies: req.cookies,
+        });
+      }
+    });
+    router.route('/login/failed').get((req, res) => {
+      res.json({
+        success: false,
+        message: 'user could not be authenticated',
+      });
+    });
+    router.route('/logout').get((req, res) => {
+      req.logout();
+      res.redirect(config.clientUrl);
+    });
   }
 }
 
--- server/server.js
+++ server/server.js
@@ -1,13 +1,13 @@
 const express = require('express');
-// const ExpressSession = require('express-session');
-const session = require('cookie-session');
+const session = require('express-session');
+// const cookieSession = require('cookie-session');
 const passport = require('passport');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
 const app = express();
 const router = express.Router();
 
-const passportStrategy = require('./strategy');
+const passportStrategy = require('./strategy/twitter');
 const config = require('./config');
 const Routes = require('./routes');
 
@@ -15,19 +15,29 @@ app.use(cookieParser());
 
 app.use(
   session({
-    name: 'arinze-session',
-    keys: ['some-secret'],
-    maxAge: config.maxAge,
+    secret: config.secret,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      maxAge: config.maxAge,
+    },
   })
 );
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.use(cors());
+app.use(
+  cors({
+    origin: 'http://localhost:3000', // allow to server to accept request from different origin
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true, // allow session cookie from browser to pass through
+  })
+);
 
 app.use('/auth', Routes.route(router));
 
 app.get('/', (req, res) => {
+  console.log(req.user);
   res.json('hello world');
 });
 
