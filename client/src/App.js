d3d1882293f3a896e50895031b9faac31bbc30ee
Arinze Obi <arinze19@outlook.com>
2023-01-23 20:32:02 +0100

    session-fix

--- client/src/App.js
+++ client/src/App.js
@@ -2,25 +2,37 @@ import React from 'react';
 import axios from 'axios';
 
 function App() {
+  const [user, setUser] = React.useState({});
+
   React.useEffect(() => {
-    console.log('I got mounted');
+    fetchUser();
   }, []);
 
   const handleClick = () => {
     window.open('http://localhost:5151/auth/twitter', '_self');
   };
 
-  const handleClick2 = async () => {
-    const data = await axios.get('http://localhost:5151');
+  const fetchUser = async () => {
+    const path = user ? 'logout' : 'login/success';
+    try {
+      const { data } = await axios.get(`http://localhost:5151/auth/${path}`, {
+        withCredentials: true,
+      });
 
-    console.log(data);
+      setUser(data.user);
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   return (
     <div className='App'>
-      <h1>Hello world</h1>
-      <button onClick={handleClick}>click</button>
-      <button onClick={handleClick2}>click json</button>
+      <h1>
+        {user
+          ? `Welcome back ${user.username} - ${user.displayName}`
+          : 'Please Login'}
+      </h1>
+      <button onClick={handleClick}>{user ? 'logout' : 'login'}</button>
     </div>
   );
 }
